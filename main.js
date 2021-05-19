66fadcff76d1cec8914b3012708966ad86f2a39d
Marina8989 <marina.mill209@gmail.com>
2021-05-19 14:41:07 -0700

    js part

---
+++ main.js
@@ -0,0 +1,10 @@
+const blockBtn = document.querySelectorAll('.fa-chevron-down');
+const btnTimes = document.querySelectorAll('.fa-times');
+const answerBlock = document.querySelectorAll('.answer');
+
+blockBtn.forEach(item => {
+    item.addEventListener('click', () => {
+         item.parentNode.classList.toggle('active');
+         item.parentNode.parentNode.classList.toggle('active');
+    })
+})
